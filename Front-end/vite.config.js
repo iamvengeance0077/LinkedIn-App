3515c33bb8171e76011414ffd7ab163220bd7afc
Emanual Joyal K A <joelemanual0077@gmail.com>
2025-10-30 21:04:54 +0530

    Update vite.config.js

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    host: true,  // allows external access
+    port: 3001, 
+  },
 })
